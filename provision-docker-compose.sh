ccd97ad6e9f557c4b8b5154e1878716deb02f362
Rui Lopes <rgl@ruilopes.com>
2021-10-09 06:24:43 +0100

    upgrade to docker compose 2.0.1

--- provision-docker-compose.sh
+++ provision-docker-compose.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
 
-docker_compose_version="${1:-1.29.2}"; shift || true
+docker_compose_version="${1:-v2.0.1}"; shift || true
 
 # download.
 # see https://github.com/docker/compose/releases
-# see https://docs.docker.com/compose/install/#install-compose-on-linux-systems
-docker_compose_url="https://github.com/docker/compose/releases/download/$docker_compose_version/docker-compose-$(uname -s)-$(uname -m)"
+# see https://docs.docker.com/compose/cli-command/#install-on-linux
+docker_compose_url="https://github.com/docker/compose/releases/download/$docker_compose_version/docker-compose-linux-$(uname -m)"
 wget -qO /tmp/docker-compose "$docker_compose_url"
 
 # install.
-install -o root -g root -m 555 /tmp/docker-compose /usr/local/bin
+install -d ~/.docker/cli-plugins
+install -m 555 /tmp/docker-compose ~/.docker/cli-plugins
 rm /tmp/docker-compose
-docker-compose --version
+docker compose version
