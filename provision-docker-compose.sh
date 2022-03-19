e497156caf5400962c250ac030d69bff1e3c4ed9
Rui Lopes <rgl@ruilopes.com>
2022-03-19 11:23:00 +0000

    upgrade to docker compose 2.3.3

--- provision-docker-compose.sh
+++ provision-docker-compose.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-docker_compose_version="${1:-v2.1.1}"; shift || true
-
-# download.
-# see https://github.com/docker/compose/releases
-# see https://docs.docker.com/compose/cli-command/#install-on-linux
-docker_compose_url="https://github.com/docker/compose/releases/download/$docker_compose_version/docker-compose-linux-$(uname -m)"
-wget -qO /tmp/docker-compose "$docker_compose_url"
-
 # install.
-install -d ~/.docker/cli-plugins
-install -m 555 /tmp/docker-compose ~/.docker/cli-plugins
-rm /tmp/docker-compose
+# see https://docs.docker.com/compose/install/#install-compose-on-linux-systems
+# see https://github.com/docker/compose/releases
+# NB the apt respository was already configured in provision-docker.sh.
+docker_compose_version='2.3.3'
+docker_compose_package_version="$(apt-cache madison docker-compose-plugin | awk "/$docker_compose_version~/{print \$3}")"
+apt-get install -y "docker-compose-plugin=$docker_compose_package_version"
 docker compose version
